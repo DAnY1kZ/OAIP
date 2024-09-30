9d4ff73a51a136274c874261462f7a968bc4eeab
DAnY1kZ <513261952z@gmail.com>
2024-09-30 12:48:59 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+# word = input("Напишите слово")
+# if word == "python":
+#     print("ДА")
+# else:
+#     print("НЕТ")
+#
+#
+# slovo1, slovo2 = input(), input()
+# if slovo1 == "Да" and slovo2 == "Да":
+#     print("ВЕРНО")
+# elif slovo2 == "Нет" and slovo1 == "Нет":
+#     print("ВЕРНО")
+# else:
+#     print("НЕВЕРНО")
+#
+#
+# one, two, three = input(), input(), input()
+# if one == "раз" and two == "два" and three == "три":
+#     print("ГОРИ")
+# elif one == "1" and two == "2" and three == "3":
+#     print("ГОРИ")
+# else:
+#     print("НЕ ГОРИ")
+#
+#
+#
+# city1, city2 = input(), input()
+# if city1 == "Тула" or city2 == "Пенза":
+#     print("ДА")
+# else:
+#     print("НЕТ")
+#
+#
+# n, m = int(input("Введите расстояние")), int(input("Введите кол-во км за день"))
+# day = n // m
+# print("На день :", day, "он добежит до финиша")
+#
+#
+# road = int(input())
+# if ((road // 100 % 100) + (road % 10)) % 8 != 0 and road // 10 % 10 == 3:
+#     print("Да")
+# else:
+#     print(road // 100 % 100 + road % 10, road //10 % 10)
+
+
+
